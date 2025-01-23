871aa6503ae72fe983d57cf59d5d11e73bf1f136
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2025-01-23 13:31:43 +0000

    update sample

--- src/test/java/io/github/fvarrui/jeppetto/tests/Main.java
+++ src/test/java/io/github/fvarrui/jeppetto/tests/Main.java
@@ -25,14 +25,20 @@ public class Main {
 //        requestCompletion.getMessages().add(new UserMessage("Hello!"));
 //        System.out.println(jeppetto.chatCompletion(requestCompletion));
 
-        Chat chat = new Chat(API_KEY, "gpt-4o-mini",
-        "Actúa como un cuentacuentos interactivo, ofreciendo opciones." +
-                "Irás ofreciendo partes pedacitos del cuento y en función de la opción elegida, te lo irás inventando." +
-                "Dame cada pedacito en formato json."
-        );
-        System.out.println("Assistant: " + chat.send("Empieza el cuento").getContent());
-        System.out.println("Assistant: " + chat.send("segunda opción").getContent());
-        System.out.println("Assistant: " + chat.send("primera opción").getContent());
+//        Chat chat = new Chat(API_KEY, "gpt-4o-mini",
+//        "Actúa como un cuentacuentos interactivo, ofreciendo opciones." +
+//                "Irás ofreciendo partes pedacitos del cuento y en función de la opción elegida, te lo irás inventando." +
+//                "Dame cada pedacito en formato json."
+//        );
+//        System.out.println("Assistant: " + chat.send("Empieza el cuento").getContent());
+//        System.out.println("Assistant: " + chat.send("segunda opción").getContent());
+//        System.out.println("Assistant: " + chat.send("primera opción").getContent());
+
+        Chat chat = new Chat(API_KEY, "gpt-4o-mini", "Actúa como un asistente amigable");
+        System.out.println("Assistant: " + chat.send("Hola!").getContent());
+        System.out.println("Assistant: " + chat.send("Háblame de Geppetto el de Pinocho").getContent());
+        System.out.println("Assistant: " + chat.send("Y quién es Pepito Grillo?").getContent());
+
 
     }
 
